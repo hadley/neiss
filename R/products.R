9e2c916b6bf12aaf0ee239784fda60305a0a9126
hadley <h.wickham@gmail.com>
2016-02-17 11:43:02 +1100

    Add product codes
    Former-commit-id: 797c0d9b8020f58586ed3a64a83a4e62a06d08ac

---
+++ R/products.R
@@ -0,0 +1,13 @@
+#' NEISS product codes
+#'
+#' Descriptions of the NEISS product codes.
+#'
+#' @source
+#'   \url{http://www.cpsc.gov/Media/Documents/Research--Statistics/NEISS-Injury-Data/Product-Code-Comparability-Table/} extracted with Tabula.
+#' @format
+#' \describe{
+#'    \item{code}{Product code}
+#'    \item{title}{Description}
+#'    \item{deleted}{Is the code still in active use?}
+#' }
+"products"
